f07f608c1825931dcfa3b2e0fdbe250390af60b0
egame_webcp <1598131255@qq.com>
2019-01-09 17:47:36 +0800

    父子组件通信

---
+++ src/components/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="index">
+    <!-- <NavList :navMsg="navMsg" @getChidValue="getValue"></NavList> -->
+    <ul>
+      <li :key="item.id" v-for="(item, index) in todos" @click="testClick(index)">
+        {{ item.text }} - {{ index }}
+      </li>
+    </ul>
+    <span  v-show="flg">{{ txt }}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      txt: '显示',
+      navMsg: '首页导航',
+      flg: false,
+      todos: [
+        { text: 'JavaScript' },
+        { text: 'Vue' },
+        { text: 'jQuery' }
+      ]
+    }
+  },
+  methods: {
+    testClick: function (ms) {
+      console.log(`我要${this.txt}${ms}`)
+      // this.flg = true
+    },
+    getValue: function (val, flag) {
+      this.txt = val
+      this.flg = flag
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/comm/Nav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="nav">
+    <div class="nav" @click="send">1111{{ navMsg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: ['navMsg'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+    send: function () {
+      this.$emit('getChidValue', '子组件回传', true)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.nav{width: 100%; text-align: center; font-size: 36px; position: fixed; top: 0}
+</style>
